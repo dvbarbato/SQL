fa8a3cffbcbbfc6cd93de7eaae59223668c435c5
david_rcsp@hotmail.com <david_rcsp@hotmail.com>
2023-05-22 17:15:36 +0000

    update function where

--- day02/04_SELECT_FROM_WHERE.sql
+++ day02/04_SELECT_FROM_WHERE.sql
@@ -18,7 +18,65 @@ WHERE vlFrete > vlPreco AND vlPreco >= 100
 
 -- COMMAND ----------
 
+-- DBTITLE 1,Filtrando produtos utilizando OR
 SELECT *
 FROM silver.olist.produto
 
-WHERE descCategoria = 'pet_shop'
+WHERE descCategoria = 'pet_shop' OR descCategoria = 'telefonia' OR descCategoria = 'bebes'
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Filtrando produtos utilizando IN
+SELECT *
+FROM silver.olist.produto
+WHERE descCategoria IN ('pet_shop', 'telefonia', 'bebes')
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Filtrando Data, mas nao está aparencendo o dia 31
+SELECT *
+FROM silver.olist.pedido
+
+WHERE dtPedido >= '2017-01-01'
+AND dtPedido <= '2017-01-31'
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Filtrando data com date para truncar a data
+SELECT idPedido,
+       idCliente,
+       descSituacao,
+       dtPedido,
+       date(dtPedido)
+
+FROM silver.olist.pedido
+
+WHERE date(dtPedido) >= '2017-01-01' AND date(dtPedido) <= '2017-01-31'
+
+-- COMMAND ----------
+
+SELECT *
+
+FROM silver.olist.pedido
+
+WHERE year(dtPedido) = 2017 AND month(dtPedido) = 1
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Mes 01 e 06 de 2017
+SELECT *
+
+FROM silver.olist.pedido
+
+WHERE year(dtPedido) = 2017
+AND (month(dtPedido) = 1
+OR month(dtPedido) = 6)
+
+-- COMMAND ----------
+
+SELECT *
+
+FROM silver.olist.pedido
+
+WHERE year(dtPedido) = 2017
+AND month(dtPedido) IN (1,6)
